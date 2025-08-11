360aa77d7291f7c356b91d4aff69dc2272d136c4
Tokslaw7 <taborg@MacBookPro.lan>
2025-08-11 18:47:26 -0400

    prisma schema

---
+++ backend-prisma/routes/activity.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const { PrismaClient } = require("../generated/prisma");
+
+const prisma = new PrismaClient();
+const activityRouter = express.Router();
+
+
+
+activityRouter.get('/activity', async (req, res)=>{
+    try{
+        const activity = await prisma.activity.findMany();
+        return res.json(activity);
+    } catch(error) {
+        console.error('Error fetching data: ', error);
+        res.status(500).json({error: 'Internal server error'});
+    }
+});
+
+activityRouter.get("activity/:id", async (req, res)=>{
+
+        const activityId = Number(req.params.activityId);
+
+        try {
+            const activity = await prisma.activity.findUnique({
+                where: {
+                    id: activityId,
+                },
+                data: {
+                    activity: true,
+                },
+            });
+
+            res.status(200).json({
+                success: true,
+                activity: activityId,
+            });
+        }catch (e) {
+
+            res.status(500).json({
+                success: false,
+                message: "Something went wrong, please try again later",
+            });
+        }
+});
+
+
+activityRouter.put ("activity/:id", async function(req, res){
+    res.status(200).json({
+        success: true,
+    });
+})
+
+activityRouter.delete('/activity', async function(req,res){
+    res.send("Delete activity").end();
+})
+
+
+module.exports = activityRouter;

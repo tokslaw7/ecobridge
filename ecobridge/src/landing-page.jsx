36adaa662a72bf34e0c7c14786d74c99431665af
Your Name <you@example.com>
2025-08-13 18:01:22 -0400

    landing page set up

---
+++ ecobridge/src/landing-page.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { BrowserRouter } from 'react-router-dom';
+// import About from "./about";
+// import Login from "./login";
+// import Signup from "./signup";
+// import tailwindcss from "@tailwindcss/vite";
+
+
+export default function landingPage(){
+    return(
+
+
+      <div>
+        domo
+      </div>
+    )
+        
+    
+
+}
+
+
+// <Router>
+        //     <nav style={style.nav}>
+        //         {/* <Link to="/about" style={style.link}> About </Link>
+        //         <Link to="/login" style={style.link}> Login</Link>
+        //         <Link to="/signup" style={style.link}> SignUp</Link> */}
+        //     </nav>
+        //     <div style={style.content}>
+        //         <Routes>
+        //         <Route path="/about" element={<About />} />
+        //         <Route path="/login" element={<Login />} />
+        //         <Route path="/signup" element={<Signup />} />
+        //         </Routes>
+        //     </div>
+        // </Router>
+    
+//     function tailwindcss(){
+//       return(
+//         <div>
+//           <button outline> about </button>
+//         </div>
+//       )
+//     }
--- ecobridge/src/main.jsx
+++ ecobridge/src/main.jsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+
+import {BrowserRouter, Routes, Route, Link}from "react-router-dom";
 import App from './App.jsx'
 
+
+const style ={
+    nav: {
+    backgroundColor: '#333',
+    padding: '1rem'
+  },
+  link: {
+    color: 'white',
+    marginRight: '1rem',
+    textDecoration: 'none',
+    fontWeight: 'bold'
+  },
+  content: {
+    padding: '2rem'
+  }
+};
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+     
+             <nav style={style.nav}>
+                 <Link to="/about" style={style.link}> About </Link>
+                <Link to="/login" style={style.link}> Login</Link>
+                <Link to="/signup" style={style.link}> SignUp</Link> 
+            </nav>
+            <div style={style.content}>
+                <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                </Routes>
+            </div>
+        
+    </BrowserRouter>
   </StrictMode>,
 )

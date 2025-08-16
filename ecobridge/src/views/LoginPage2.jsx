bcf4ff3d817b190674c1dd6eb4fbfdd833bb0e2b
Starfoxxy <athenajacob97@gmail.com>
2025-08-15 22:58:36 -0400

    login component added

---
+++ ecobridge/src/views/LoginPage2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Login2 from '../component/login/Login2'
+
+export default function LoginPage2() {
+    return (
+       <div>
+           <Login2 />
+       </div>
+    )
+}

e3db176193222c6e48cdc4af1ff482f75096768f
Your Name <you@example.com>
2025-08-20 17:46:40 -0400

    added landing page structer, placeholder image not work

--- ecobridge/src/component/landingpage/Landing.jsx
+++ ecobridge/src/component/landingpage/Landing.jsx
@@ -10,28 +10,35 @@
 export default function Landing(){
     return(
       <>
-      <div>
-        <div>
-          <h1>Art. Music. Self.</h1>
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-          <section>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <button> Join </button>
-            </section>
-      </div>
-      <div>
-        <section>
-          <p> Music </p>
-          <div/>
+      <div class="container my-5">
+  <div class="row align-items-center">
+  <div class="col-md-6 text-center">
+      <img src="/placeholder.jpg" class="img-thumbnail" alt="placeholder" />
+    </div>
+    <div class="col-md-6 text-end">
+      <h1>Art. Music. Self.</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <section>
+      <button class="btn btn-primary">Join</button>
+      <button class="btn btn-outline-primary"> Explore</button>
+      </section>
+    </div>
+  </div>
+</div>
+
+      <div class="container text-center row p-4 gap-3">
+        <section class="col-md bg-danger text-primary-emphasis" >
+          <p class="h5"> Music </p>
+        <p class="text-primary-emphasis">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </section>
-        <section>
-        <p> Art </p>
-        <div/>
+        <section class="col-md bg-info text-primary-emphasis">
+        <p class="h5"> Art </p>
+      <p class="text-primary-emphasis">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </section>
-       <section>
-        <p>Story </p>
-        <div/>
+       <section class="col-md bg-warning text-primary-emphasis">
+        <p class="h5">Story </p>
+        
+        <p class="text-primary-emphasis">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </section>
         
       </div>
